f1b04b27a86242823ee7c3912edc43890e48f05c
shatytskyi <shatytskyi@gmail.com>
2025-09-01 01:37:49 +0200

    compose redesign

--- app/src/main/java/com/shatytskyi/munchcounter/ui/theme/MunchkinTheme.kt
+++ app/src/main/java/com/shatytskyi/munchcounter/ui/theme/MunchkinTheme.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -18,9 +21,32 @@ val LocalMunchkinTypography = staticCompositionLocalOf { MunchkinTypography() }
 @Composable
 fun MunchkinTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) DarkMunchkinColors else LightMunchkinColors
+    val colors = when {
+        dynamicColor -> {
+            val context = LocalContext.current
+            val dynamicColorScheme = if (darkTheme) {
+                dynamicDarkColorScheme(context)
+            } else {
+                dynamicLightColorScheme(context)
+            }
+
+            MunchkinColors(
+                primary = dynamicColorScheme.primary,
+                secondary = dynamicColorScheme.secondary,
+                green = dynamicColorScheme.tertiary,
+                red = dynamicColorScheme.error,
+                background = dynamicColorScheme.background,
+                onBackground = dynamicColorScheme.onBackground,
+                grey = dynamicColorScheme.outline,
+            )
+        }
+
+        darkTheme -> DarkMunchkinColors
+        else -> LightMunchkinColors
+    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
