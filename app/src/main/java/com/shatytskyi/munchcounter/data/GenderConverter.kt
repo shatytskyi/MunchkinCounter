0a1cf55cf3f11745c8e6192d6dc3146dbe2be1a3
shatytskyi <shatytskyi@gmail.com>
2025-09-01 14:00:36 +0200

    compose redesign

---
+++ app/src/main/java/com/shatytskyi/munchcounter/data/GenderConverter.kt
@@ -0,0 +1,15 @@
+package com.shatytskyi.munchcounter.data
+
+import androidx.room.TypeConverter
+
+class GenderConverter {
+    @TypeConverter
+    fun fromGender(gender: Gender): String {
+        return gender.name
+    }
+
+    @TypeConverter
+    fun toGender(genderString: String): Gender {
+        return Gender.fromString(genderString)
+    }
+}
